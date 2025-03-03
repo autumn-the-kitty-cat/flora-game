acbebf0a120f8acbacda6afa1e1f3a1ff8452309
autumn-the-kitty-cat <theodorebaker08@gmail.com>
2025-03-03 15:03:29 -0500

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+    pkgs = import <nixpkgs> {};
+in
+pkgs.mkShell {
+    packages = with pkgs; [
+        clang
+
+        raylib
+
+        libGL
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXrandr
+        xorg.libXrender
+    ];
+
+    RAYLIB = "${pkgs.raylib}";
+}
