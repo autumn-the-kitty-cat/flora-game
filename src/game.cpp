833ad14be5201340a81f99d96b970a930698cd2a
autumn-the-kitty-cat <theodorebaker08@gmail.com>
2025-03-04 14:18:34 -0500

    configureRaylib function | make player move and fall

--- src/game.cpp
+++ src/game.cpp
@@ -3,6 +3,7 @@
 
 #include "level.cpp"
 #include "player.cpp"
+#include "terrain.cpp"
 
 #include <raylib.h>
 
@@ -14,18 +15,30 @@ class Game {
 private:
     GameState state;
     Player player;
+    Terrain terrain;
     void loop()
     {
+        ClearBackground(BLACK);
         player.draw();
+        player.move();
+        player.fall();
     };
+
+    void configureRaylib()
+    {
+        SetConfigFlags(FLAG_VSYNC_HINT);
+        SetExitKey(KEY_NULL);
+        ToggleFullscreen();
+    }
 
 public:
     void start(std::string title)
     {
         InitWindow(0, 0, title.c_str());
-        SetExitKey(KEY_NULL);
-        ToggleFullscreen();
+        configureRaylib();
+
         player.sprite.load("resources/player.png");
+
         while (!WindowShouldClose()) {
             BeginDrawing();
             loop();
