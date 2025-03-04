ecbe8fb216a3364fb3a16a7fa2219572af02cb11
autumn-the-kitty-cat <theodorebaker08@gmail.com>
2025-03-04 14:56:19 -0500

    terrain draw and init functions | raw is slice of optional<Block>

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -1,10 +1,12 @@
 #ifndef TERRAIN
 #define TERRAIN
 
+#include <optional>
 #define WIDTH 20
 #define HEIGHT 12
 
 #include "sprite.cpp"
+#include <raylib.h>
 
 class BlockType {
 public:
@@ -18,11 +20,37 @@ public:
 };
 
 class Terrain {
-    Block raw[WIDTH][HEIGHT];
+    std::optional<Block> raw[WIDTH][HEIGHT];
 
 public:
+    void init()
+    {
+        Sprite grassSprite;
+        grassSprite.load("resources/grass.png");
+        Block grass = Block {
+            .type = {
+                .sprite = grassSprite,
+                .isFatal = false,
+            },
+        };
+
+        for (unsigned int i = 0; i < WIDTH; i++) {
+            raw[i][10] = std::make_optional(grass);
+        }
+    }
     void draw()
     {
+        int blockWidth = GetScreenWidth() / WIDTH;
+        for (unsigned int i = 0; i < WIDTH; i++) {
+            for (unsigned int j = 0; j < HEIGHT; j++) {
+                std::optional<Block> block = raw[i][j];
+                if (block.has_value()) {
+                    Texture blockTexture = block.value().type.sprite.texture;
+
+                    DrawTexture(blockTexture, blockWidth * i, blockWidth * j, WHITE);
+                }
+            }
+        }
     }
 };
 
